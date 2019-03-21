202dd5952d82689d52b11dd9db77ce017c74a731
marioa98 <mmartinez53@ucol.mx>
2019-03-21 09:54:06 -0600

    Clase dotnet ordenamientos

---
+++ Ordenamiento/Burbuja.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.text;
+
+namespace Ordenamiento
+{
+    public class Burbuja
+    {
+        public void Ordenar(int[] arr){
+            throw new NotImplementedException("Lo que sea PRUEBAS");
+        }
+    }
+}
